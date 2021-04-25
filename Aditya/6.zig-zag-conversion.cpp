3ca8fd1aae90f1d3b50dcc4ea3769ce19e5db04b
Aditya Prakash <prakashadityaap@gmail.com>
2021-04-25 19:23:34 +0530

    initial commit for ques6

---
+++ Aditya/6.zig-zag-conversion.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=6 lang=cpp
+ *
+ * [6] ZigZag Conversion
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        
+    }
+};
+// @lc code=end
+
