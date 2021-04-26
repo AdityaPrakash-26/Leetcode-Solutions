36e0f3392937c423f7b8c32d837dbcb59454df38
Aditya Prakash <prakashadityaap@gmail.com>
2021-04-26 13:04:11 +0530

    Added ques 6

--- Aditya/6.zig-zag-conversion.cpp
+++ Aditya/6.zig-zag-conversion.cpp
@@ -5,11 +5,24 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string convert(string s, int numRows) {
-        
+    string convert(string s, int numRows)
+    {
+        vector<string> vs(numRows, "");
+        int n = s.length(), i = 0;
+        while (i < n)
+        {
+            for (int j = 0; j < numRows && i < n; j++)
+                vs[j].push_back(s[i++]);
+            for (int j = numRows - 2; j >= 1 && i < n; j--)
+                vs[j].push_back(s[i++]);
+        }
+        string zigzag;
+        for (string v : vs)
+            zigzag += v;
+        return zigzag;
     }
 };
 // @lc code=end
-
