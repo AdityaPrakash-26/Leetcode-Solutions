0ad1fb9217f4e98b598f862ffa5df8f85b6a9dd8
Aditya Prakash <prakashadityaap@gmail.com>
2021-04-26 13:37:26 +0530

    Added Ques 9

---
+++ Aditya/9.palindrome-number.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0))
+        {
+            return false;
+        }
+
+        int rev = 0;
+        while (x > rev)
+        {
+            rev *= 10;
+            rev += x % 10;
+            x /= 10;
+        }
+
+        return x == rev || x == rev / 10;
+    }
+};
+// @lc code=end
