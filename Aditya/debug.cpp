eae3d4ae080eb32c658f434482691ebc7966a920
Aditya Prakash <prakashadityaap@gmail.com>
2021-04-26 19:43:17 +0530

    improved ques 8

--- Aditya/debug.cpp
+++ Aditya/debug.cpp
@@ -2,39 +2,65 @@
 #include <vector>
 using namespace std;
 
-int reverse(int x)
+int myAtoi(string s)
 {
-    bool flag = true;
-    if (x < 0)
-    {
-        flag = false;
-        x *= -1;
-    }
-    long long ans = 0;
-    while (x > 0)
-    {
-        ans *= 10;
-        ans += x % 10;
-        x /= 10;
-    }
-    if (INT_MIN < ans < INT_MAX)
-    {
-        if (flag == false)
-        {
-            return (ans * -1);
-        }
-        else
-        {
-            return ans;
-        }
-    }
-    else
-    {
+    int i = 0;
+    if (s.length() == 0)
         return 0;
+
+    while (i < s.length() && s[i] == ' ')
+        i++;
+    int num = 0;
+    while (i < s.length())
+    {
+        switch (s.at(i))
+        {
+        case '0':
+            num += 0;
+            num *= 10;
+            i++;
+        case '1':
+            num += 1;
+            num *= 10;
+            i++;
+        case '2':
+            num += 2;
+            num *= 10;
+            i++;
+        case '3':
+            num += 3;
+            num *= 10;
+            i++;
+        case '4':
+            num += 4;
+            num *= 10;
+            i++;
+        case '5':
+            num += 5;
+            num *= 10;
+            i++;
+        case '6':
+            num += 6;
+            num *= 10;
+            i++;
+        case '7':
+            num += 7;
+            num *= 10;
+            i++;
+        case '8':
+            num += 8;
+            num *= 10;
+            i++;
+        case '9':
+            num += 9;
+            num *= 10;
+            i++;
+        }
     }
+    return num;
 }
 
 int main()
 {
-    cout << reverse(1534236469) << endl;
+    cout << atoi("42") << endl;
 }
