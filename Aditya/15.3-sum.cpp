9830ea0de5d066c7f07019d235436e8549fa35b8
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-02 12:40:10 +0530

    initial commit for q15

---
+++ Aditya/15.3-sum.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=15 lang=cpp
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
