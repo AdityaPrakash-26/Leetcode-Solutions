9fe366edd2e0b3a86a731cdf3a11131476ab8037
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-03 08:47:10 +0530

    ques 16 initial commit

---
+++ Aditya/16.3-sum-closest.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=16 lang=cpp
+ *
+ * [16] 3Sum Closest
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
