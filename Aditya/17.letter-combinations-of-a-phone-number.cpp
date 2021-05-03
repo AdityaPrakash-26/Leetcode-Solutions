798b9685e508eb789b8fb823d30f1f50f5676bf7
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-03 16:09:35 +0530

    Added q17

--- Aditya/17.letter-combinations-of-a-phone-number.cpp
+++ Aditya/17.letter-combinations-of-a-phone-number.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+25/25 cases passed (0 ms)
+Your runtime beats 100 % of cpp submissions
+Your memory usage beats 35.68 % of cpp submissions (6.6 MB)
+*/
+
+/*
  * @lc app=leetcode id=17 lang=cpp
  *
  * [17] Letter Combinations of a Phone Number
@@ -7,8 +14,26 @@
 // @lc code=start
 class Solution {
 public:
+    const vector<string> pad = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+    
     vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+		vector<string> result;
+        result.push_back("");
         
+        for(auto digit: digits) {
+            vector<string> tmp;
+            for(auto candidate: pad[digit - '0']) {
+                for(auto s: result) {
+                    tmp.push_back(s + candidate);
+                }
+            }
+            result.swap(tmp);
+        }
+        return result;
     }
 };
 // @lc code=end
