41e121de26a7e0238e7f3b99aa007c66cabd8758
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-09 10:19:49 +0530

    q21

--- Aditya/21.merge-two-sorted-lists.cpp
+++ Aditya/21.merge-two-sorted-lists.cpp
@@ -1,4 +1,11 @@
 /*
+* Accepted
+* 208/208 cases passed (16 ms)
+* Your runtime beats 24.88 % of cpp submissions
+* Your memory usage beats 15.19 % of cpp submissions (14.9 MB)
+*/
+
+/*
  * @lc app=leetcode id=21 lang=cpp
  *
  * [21] Merge Two Sorted Lists
@@ -18,7 +25,22 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        
+        ListNode n(INT_MIN);
+        ListNode *p = &n;
+        while(l1 && l2){
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+
+        p->next = l1 ? l1 : l2;
+        return n.next;
+
     }
 };
 // @lc code=end
