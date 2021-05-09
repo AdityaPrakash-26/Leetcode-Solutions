f973972d29810ab8b9aa4a9c8245e2388c78eac5
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-09 10:06:42 +0530

    q20

--- Aditya/20.valid-parentheses.cpp
+++ Aditya/20.valid-parentheses.cpp
@@ -5,10 +5,42 @@
  */
 
 // @lc code=start
+
+#include<stack> 
+
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<char> stk;
+
+        if(s.length()%2!=0){
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.at(i) == '(' || s.at(i) == '{' || s.at(i) == '['){
+                stk.push(s.at(i));
+            } else {
+                if(!stk.empty()){
+                    if(s.at(i) == ')' && stk.top() =='('){
+                        stk.pop();
+                    } else if(s.at(i) == '}' && stk.top() =='{'){
+                        stk.pop();
+                    } else if(s.at(i) == ']' && stk.top() =='['){
+                        stk.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        if(stk.empty()){
+            return true;
+        } else {
+            return false;
+        }
     }
 };
 // @lc code=end
