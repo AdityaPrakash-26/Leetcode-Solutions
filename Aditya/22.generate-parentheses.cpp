20a5de9661fccc262f99f9ab395e78c475a52dce
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-09 17:31:38 +0530

    q22

---
+++ Aditya/22.generate-parentheses.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=22 lang=cpp
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        
+    }
+};
+// @lc code=end
+
