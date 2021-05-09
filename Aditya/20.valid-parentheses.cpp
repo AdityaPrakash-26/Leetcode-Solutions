765b77a6276949b37a23102571d4a475d0c16712
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-09 09:54:25 +0530

    q20

---
+++ Aditya/20.valid-parentheses.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=20 lang=cpp
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isValid(string s) {
+        
+    }
+};
+// @lc code=end
+
