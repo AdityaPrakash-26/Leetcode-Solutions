55e38779fd4253cb68731583a577247f8aa41f0b
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-11 17:09:48 +0530

    q8

--- Aditya/8.string-to-integer-atoi.cpp
+++ Aditya/8.string-to-integer-atoi.cpp
@@ -9,7 +9,10 @@ class Solution
 {
 public:
     int myAtoi(string s){
-        
+        int n = s.size();
+        int left = 0, right = n - 1;
+        int sign = 1;
+        double temp_res = 0, multiplier = 1;
     }
 };
 // @lc code=end
