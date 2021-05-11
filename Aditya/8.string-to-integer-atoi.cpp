18b9decad909841759444e9feb63c4fa57845e2e
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-11 18:32:05 +0530

    q8

--- Aditya/8.string-to-integer-atoi.cpp
+++ Aditya/8.string-to-integer-atoi.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+1082/1082 cases passed (4 ms)
+Your runtime beats 58.56 % of cpp submissions
+Your memory usage beats 22.23 % of cpp submissions (7 MB)
+*/
+
+/*
  * @lc app=leetcode id=8 lang=cpp
  *
  * [8] string to Integer (atoi)
@@ -9,20 +16,33 @@ class Solution
 {
 public:
     int myAtoi(string s){
-        long result = 0;
-        int indicator = 1;
-        for(int i = 0; i<s.size();){
-            i = s.find_first_not_of(' ');
-            if(s[i] == '-' || s[i] == '+')
-                indicator = (s[i++] == '-')? -1 : 1;
-            while('0'<= s[i] && s[i] <= '9') 
-            {
-                result = result*10 + (s[i++]-'0');
-                if(result*indicator >= INT_MAX) return INT_MAX;
-                if(result*indicator <= INT_MIN) return INT_MIN;                
-            }
-            return result*indicator;
+        int32_t i, ans, sign, backup;
+        i=0;
+        while(i<s.length() && s[i] == ' ')
+            i++;
+        sign = 1; // positive
+        if(i<s.length() && s[i] == '+')
+        {
+            i++;
         }
+        else if(i<s.length() && s[i] == '-')
+        {
+            sign = -1; // negative
+            i++;
+        }
+        ans=0;
+        while(i<s.length() && isdigit(s[i]))
+        {
+            backup = ans;
+            if(sign==1 && ans > (INT_MAX-(s[i]-'0'))/10)
+                return INT_MAX;
+            else if(sign==-1  && ans > (INT_MAX-(s[i]-'0'))/10)
+                return INT_MIN;
+            
+            ans = ans*10+(s[i]-'0');
+            i++;
+        }
+        return ans*sign;
     }
 };
 // @lc code=end
