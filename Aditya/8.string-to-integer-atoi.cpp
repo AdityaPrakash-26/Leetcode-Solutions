536c66bf8b1055e7940e5715299a1b503ab441dc
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-11 18:27:11 +0530

    q8

--- Aditya/8.string-to-integer-atoi.cpp
+++ Aditya/8.string-to-integer-atoi.cpp
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode id=8 lang=cpp
  *
- * [8] String to Integer (atoi)
+ * [8] string to Integer (atoi)
  */
 
 // @lc code=start
@@ -11,14 +11,13 @@ public:
     int myAtoi(string s){
         long result = 0;
         int indicator = 1;
-        for(int i = 0; i<str.size();)
-        {
-            i = str.find_first_not_of(' ');
-            if(str[i] == '-' || str[i] == '+')
-                indicator = (str[i++] == '-')? -1 : 1;
-            while('0'<= str[i] && str[i] <= '9') 
+        for(int i = 0; i<s.size();){
+            i = s.find_first_not_of(' ');
+            if(s[i] == '-' || s[i] == '+')
+                indicator = (s[i++] == '-')? -1 : 1;
+            while('0'<= s[i] && s[i] <= '9') 
             {
-                result = result*10 + (str[i++]-'0');
+                result = result*10 + (s[i++]-'0');
                 if(result*indicator >= INT_MAX) return INT_MAX;
                 if(result*indicator <= INT_MIN) return INT_MIN;                
             }
