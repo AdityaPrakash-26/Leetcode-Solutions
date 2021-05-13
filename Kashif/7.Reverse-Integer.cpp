d5d05ce6b1a0c4f7c82ef1429091b4b432c2eeeb
Kashif Raza <kashifraza08012001@gmail.com>
2021-05-13 11:42:25 +0530

    Update 7.Reverse-Integer.cpp

--- Kashif/7.Reverse-Integer.cpp
+++ Kashif/7.Reverse-Integer.cpp
@@ -11,11 +11,11 @@ public:
         temp=x;
         while(temp!=0)
         {
-        r=temp%10;
-        if (a > INT_MAX/10 || (a == INT_MAX / 10 && r > 7)) return 0;
-        if (a < INT_MIN/10 || (a == INT_MIN / 10 && r < -8)) return 0;
-        a=a*10+r;
-        temp/=10;
+             r=temp%10;
+             if (a > INT_MAX/10 || (a == INT_MAX / 10 && r > 7)) return 0;
+             if (a < INT_MIN/10 || (a == INT_MIN / 10 && r < -8)) return 0;
+             a=a*10+r;
+             temp/=10;
         }
         return a;
     }
