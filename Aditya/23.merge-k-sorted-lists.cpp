641b0395e9f0333a7c8a8199c5dba074890ea6cf
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-22 11:42:38 +0530

    q23

--- Aditya/23.merge-k-sorted-lists.cpp
+++ Aditya/23.merge-k-sorted-lists.cpp
@@ -18,7 +18,27 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+        if(lists.empty()){
+            return nullptr;
+        }
+        //todo: use the function
+    }
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if(l1 == nullptr){
+            return l2;
+        }
+
+        if(l2 == nullptr){
+            return l1;
+        }
+
+        if(l1->val <= l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else{
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
     }
 };
 // @lc code=end
