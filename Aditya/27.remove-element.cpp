39048525c67b91466e7bdbb02832a6fc5d9292f8
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-23 19:06:42 +0530

    q27

--- Aditya/27.remove-element.cpp
+++ Aditya/27.remove-element.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+113/113 cases passed (4 ms)
+Your runtime beats 47.23 % of cpp submissions
+Your memory usage beats 24.75 % of cpp submissions (8.9 MB)
+*/
+
+/*
  * @lc app=leetcode id=27 lang=cpp
  *
  * [27] Remove Element
