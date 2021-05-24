4be80448310945d33065d989c235b2a20eeaf68f
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-24 18:25:38 +0530

    q28

---
+++ Aditya/28.implement-str-str.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=28 lang=cpp
+ *
+ * [28] Implement strStr()
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for(int i = 0; i<haystack.length(); i++){
+            if(haystack.at(i) == needle.at(0)){
+                
+            }
+        }
+    }
+};
+// @lc code=end
+
