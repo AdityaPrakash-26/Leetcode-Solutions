2fc43e28adbab7bf205ad92cedfe3c7533229bc4
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-24 18:31:20 +0530

    q28

--- Aditya/28.implement-str-str.cpp
+++ Aditya/28.implement-str-str.cpp
@@ -8,10 +8,29 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for(int i = 0; i<haystack.length(); i++){
+        int i = 0;
+        bool flag = false;
+        for(i; i<haystack.length(); i++){
             if(haystack.at(i) == needle.at(0)){
-                
+                int j = i;
+                int k = 0;
+                while(needle.at(k)<needle.size() && haystack.at(j)<haystack.size()){
+                    if(haystack.at(j) != needle.at(k)){
+                        flag = true;
+                        break;
+                    }
+                    j++;
+                    k++;
+                }
+                if(flag == false){
+                    break;
+                }
             }
+        }
+        if(flag == true){
+            return i;
+        } else {
+            return -1;
         }
     }
 };
