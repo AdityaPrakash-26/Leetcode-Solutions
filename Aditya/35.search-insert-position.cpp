e6122e5b2530a0a002e61eb809568dd5045f96e5
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-30 09:48:02 +0530

    q35

---
+++ Aditya/35.search-insert-position.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+    }
+};
+// @lc code=end
+
