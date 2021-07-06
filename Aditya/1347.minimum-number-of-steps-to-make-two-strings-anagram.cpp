f969a8d174ce1c7134eeecf00ef9dc31f914a4fd
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-06 10:02:22 +0530

    q67

---
+++ Aditya/1347.minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1347 lang=cpp
+ *
+ * [1347] Minimum Number of Steps to Make Two Strings Anagram
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        
+    }
+};
+// @lc code=end
+
