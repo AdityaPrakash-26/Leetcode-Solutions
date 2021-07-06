f413c109ba6c847d420330713b0ddc386054abca
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-06 10:38:06 +0530

    q16

--- Aditya/0010-0099/16.3-sum-closest.cpp
+++ Aditya/0010-0099/16.3-sum-closest.cpp
@@ -8,9 +8,28 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int closestSum = INT_MAX;
+        int minDiff = INT_MAX;
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for(int i=0;i<size-2;i++){
+            int j = i+1;
+            int k = size-1;
 
-        
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(abs(target-sum) < minDiff){
+                    minDiff = abs(target-sum);
+                    ans = sum;
+                }
+                if(sum > target){
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
