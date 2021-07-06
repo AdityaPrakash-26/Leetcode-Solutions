8b3d198ce643b241ddc60e9a7e0bc7168a9067eb
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-06 10:23:54 +0530

    q15

--- Aditya/15.3-sum.cpp
+++ Aditya/15.3-sum.cpp
@@ -14,36 +14,46 @@ Your memory usage beats 62.03 % of cpp submissions (20.1 MB)
 // @lc code=start
 class Solution {
 public:
+
+    set<vector<int>> convertToSet(vector<vector<int>> v){
+    // Declaring the  set
+    set<vector<int>> s;
+    // Traverse the Vector
+    for (vector<int> x : v) {
+  
+        // Insert each element
+        // into the Set
+        s.insert(x);
+    }
+  
+    // Return the resultant Set
+    return s;
+}
+
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int size = nums.size();
-        vector<vector<int>> output;
-        for(int i = 0; i<size-2; i++){
-            int currNum = nums[i];
-            int l = i+i;
-            int r = size-1;
-            while(l<r){
-                int sum = currNum + nums[l] + nums[r];
-
-                if(sum==0){
-                    vector<int> temp;
-                    temp.push_back(currNum);
-                    temp.push_back(nums[l]);
-                    temp.push_back(nums[r]);
-
-                    output.push_back(temp);
-                    l++;
-                    r--;
-                } else if(sum>0) {
-                    r--;
-                } else {
-                    l++;
-                }
+        if(nums.size() < 3) return {};
+        vector<vector<int>> ret;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;++i){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int tar = 0 - nums[i],hi=nums.size()-1,lo=i+1;
+            while(hi>lo){
+                int summ = nums[hi]+nums[lo];
+                if(summ > tar)
+                    hi--;
+                else if(summ < tar)
+                    lo++;
+                else{
+                    ret.push_back({nums[i],nums[lo],nums[hi]});
+                    lo++,hi--;
+                    while(hi>lo && nums[hi]==nums[hi+1]) hi--;
+                    while(hi>lo && nums[lo]==nums[lo-1]) lo++;
+                }    
             }
         }
-        output.erase( unique( output.begin(), output.end() ), output.end() );
-        return output;
+        return ret;
     }
+  
 };
 // @lc code=end
 
