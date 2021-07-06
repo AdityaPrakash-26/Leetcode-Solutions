2fdc92f5962cf5ad10d5092866f03bce839a5b85
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-06 11:18:22 +0530

    q19

--- Aditya/19.remove-nth-node-from-end-of-list.cpp
+++ Aditya/19.remove-nth-node-from-end-of-list.cpp
@@ -1,3 +1,9 @@
+/*
+Accepted
+208/208 cases passed (8 ms)
+Your runtime beats 24.81 % of cpp submissions
+Your memory usage beats 70.44 % of cpp submissions (10.6 MB)
+*/
 
 /*
  * @lc app=leetcode id=19 lang=cpp
