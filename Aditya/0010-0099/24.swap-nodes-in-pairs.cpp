3a032d5f11add07e1736b371c8a71c6620870a78
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-07 11:24:38 +0530

    stuff

--- Aditya/0010-0099/24.swap-nodes-in-pairs.cpp
+++ Aditya/0010-0099/24.swap-nodes-in-pairs.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+55/55 cases passed (0 ms)
+Your runtime beats 100 % of cpp submissions
+Your memory usage beats 90.46 % of cpp submissions (7.4 MB)
+*/
+
+/*
  * @lc app=leetcode id=24 lang=cpp
  *
  * [24] Swap Nodes in Pairs
@@ -18,7 +25,13 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        
+        if(!head || !head->next) return head;
+
+        ListNode *temp = head->next;
+        head->next = swapPairs(temp->next); //the head should be connected to the remaining linked list. 1 should be connected to 4 in example
+        temp->next = head;  //2 should be connected to 1.
+
+        return temp;
     }
 };
 // @lc code=end
