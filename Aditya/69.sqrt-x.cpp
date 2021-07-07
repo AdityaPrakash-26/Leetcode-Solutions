0fdf4d4b73260e71e817d7893e8e86f888cbf3e3
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-07 10:34:03 +0530

    q69

--- Aditya/69.sqrt-x.cpp
+++ Aditya/69.sqrt-x.cpp
@@ -8,7 +8,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        int leftPtr = 0;
+        int rightPtr = x/2;
+
+        if(x<2){
+            return x;
+        }
+
+        int low = 0, high = x/2;
+        long long int mid;
+        while (low <= high){
+            mid = low + (high-low)/2;
+            if (mid * mid == x){
+                return mid;
+            } else if (mid * mid < x){
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return low-1;
     }
 };
 // @lc code=end
