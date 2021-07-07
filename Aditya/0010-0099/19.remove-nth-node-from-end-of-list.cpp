dbe21ffca8a0417a5f0e6c038e7b6a85c73d3631
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-07 10:26:38 +0530

    q69

--- Aditya/0010-0099/19.remove-nth-node-from-end-of-list.cpp
+++ Aditya/0010-0099/19.remove-nth-node-from-end-of-list.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+208/208 cases passed (8 ms)
+Your runtime beats 24.81 % of cpp submissions
+Your memory usage beats 70.44 % of cpp submissions (10.6 MB)
+*/
+
+/*
  * @lc app=leetcode id=19 lang=cpp
  *
  * [19] Remove Nth Node From End of List
@@ -18,27 +25,11 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* ans;
-        ListNode* t1 = head;
-        ListNode* t2 = head;
-
-        for(int i = 0; i<n; i++){
-            t1 = t1->next; 
-        }
-
-        if(t1==NULL){   
-            ans = head->next;
-            return ans;     
-        }
-
-        t1=t1.next;
-    
-        while(t1!=null){
-            t1=t1->next;
-            t2=t2->next;
-        }
-
-        t2->next=t2->next->next;
+        ListNode *fast = head, *slow = head;
+        for (int i = 0; i < n; i++) fast = fast->next;
+        if (!fast) return head->next;
+        while (fast->next) fast = fast->next, slow = slow->next;
+        slow->next = slow->next->next;
         return head;
     }
 };
---
+++ Aditya/69.sqrt-x.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=69 lang=cpp
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+    }
+};
+// @lc code=end
+
