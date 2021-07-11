790bd050b0b6c9f48b3fe33a5752802201c84c1b
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-11 09:00:25 +0530

    q1 and q19

--- Aditya/0001-0009/1.two-sum.cpp
+++ Aditya/0001-0009/1.two-sum.cpp
@@ -19,22 +19,15 @@ Your memory usage beats 79.76 % of cpp submissions (8.8 MB)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i<nums.size(); i++){
-            int first = nums[i];
-            int second = target-nums[i];
-            for(int j = i+1; j<nums.size(); j++){
-                if(nums[j]==second){
-                    vector<int> ans;
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                } else {
-                    //do nothing
-                }
-            }
+    unordered_map<int, int> tmp;
+
+    for (int i = 0; i < nums.size(); ++i) {
+        if (tmp.count(target - nums[i])) {
+            return {tmp[target - nums[i]], i};
         }
-        vector<int> wrongAnswer = {-1};
-        return wrongAnswer;
+        tmp[nums[i]] = i;
     }
+    return {nums.size(), nums.size()};
+}
 };
 // @lc code=end
--- Aditya/0010-0099/19.remove-nth-node-from-end-of-list.cpp
+++ Aditya/0010-0099/19.remove-nth-node-from-end-of-list.cpp
@@ -25,11 +25,20 @@ Your memory usage beats 70.44 % of cpp submissions (10.6 MB)
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode *fast = head, *slow = head;
-        for (int i = 0; i < n; i++) fast = fast->next;
-        if (!fast) return head->next;
-        while (fast->next) fast = fast->next, slow = slow->next;
-        slow->next = slow->next->next;
+        ListNode *p1 = head, *p2 = head;
+        for (int i = 0; i < n; i++) {
+            p1 = p1->next;
+        }
+        if (!p1) {
+            return head->next;
+        }
+
+        while (p1->next) {
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        
+        p2->next = p2->next->next;
         return head;
     }
 };
