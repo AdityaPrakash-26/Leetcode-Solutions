c57bcb93c9754ad5319a315fc03c609b7fe64dd7
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-14 12:16:50 +0530

    q41

--- Aditya/0010-0099/41.first-missing-positive.cpp
+++ Aditya/0010-0099/41.first-missing-positive.cpp
@@ -15,9 +15,9 @@ Your memory usage beats 17.11 % of cpp submissions (83 MB)
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        //the missing number will always be less than or equal to the 
-        //size of the vector
-        //place all positive elements less than or equal to the size of 
+        // the missing number will always be less than or equal to the 
+        // size of the vector
+        // place all positive elements less than or equal to the size of 
         // the vector to their respective index in the vector, e.g., 1 
         // at index 0
         
@@ -26,14 +26,14 @@ public:
             while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
                 swap(nums[i], nums[nums[i] - 1]); //place element x at index num[x - 1]
         
-        //if the number at index i is not i + 1, the missing number 
-        //is i + 1
+        // if the number at index i is not i + 1, the missing number 
+        // is i + 1
         for(int i = 0; i < n; i++)
             if(nums[i] != i + 1)
                 return i + 1;
         
-        //else, the missing number is one more than the size of the 
-        //vector
+        // else, the missing number is one more than the size of the 
+        // vector
         return n + 1;
     }
 };
