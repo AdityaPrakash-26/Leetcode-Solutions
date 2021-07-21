7cb846b93bc03a23ae3cbe4c8e0ab55040045b00
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-21 14:20:08 +0530

    q5 redone

--- Aditya/0001-0009/5.longest-palindromic-substring.cpp
+++ Aditya/0001-0009/5.longest-palindromic-substring.cpp
@@ -1,12 +1,9 @@
 /*
-Accepted
-176/176 cases passed (32 ms)
-Your runtime beats 69.6 % of cpp submissions
-Your memory usage beats 44.43 % of cpp submissions (25 MB)
+ * Accepted
+ * 177/177 cases passed (60 ms)
+ * Your runtime beats 66.91 % of cpp submissions
+ * Your memory usage beats 25.22 % of cpp submissions (231.2 MB)
 */
-
-#include <string> //added to fix vscode intellisense errors
-using namespace std;
 
 /*
  * @lc app=leetcode id=5 lang=cpp
@@ -17,31 +14,39 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n=s.length();
-        int l=1;
-        if(n<2) {
-            return s;
+    pair<int, int> getLongestPalindromeFrom(string s, int leftIdx, int rightIdx){
+        while(leftIdx >= 0 && rightIdx < s.size()){
+            if(s[leftIdx] != s[rightIdx]){
+                break;
+            } else {
+                leftIdx--;
+                rightIdx++;
+            }
         }
-
-        string ans="";
-        ans += s[0];
-
-        for(int mid=0; mid<n; ++mid){
-            Palindrome(mid-1,mid,s,ans,l,n);    //for even
-            Palindrome(mid-1,mid+1,s,ans,l,n);  //for odd
-        }
-        return ans;
+        return make_pair(leftIdx + 1, rightIdx - 1);
     }
 
-    void Palindrome(int i,int j,string &s, string &ans, int &l,int n){
-        while(i>=0 && j<n && s[i]==s[j]){
-            if((j-i+1)>l){
-                ans=s.substr(i,j-i+1);
-                l=j-i;
+    string longestPalindrome(string s) {
+        int currentLongestLength = 0;
+        pair<int, int> indexOfLongestSubString = make_pair(0, 0);
+
+        for(int i = 1; i<s.size(); i++){
+            pair<int, int> odd = getLongestPalindromeFrom(s, i-1, i+1);
+            pair<int, int> even = getLongestPalindromeFrom(s, i-1, i);
+
+            int lengthOfOdd = odd.second - odd.first + 1;   //+ 1 is done for index
+            int lengthOfEven = even.second - even.first + 1;
+
+            if(lengthOfOdd > lengthOfEven && lengthOfOdd > currentLongestLength){
+                currentLongestLength = lengthOfOdd;
+                indexOfLongestSubString = odd;
+            } else if(lengthOfEven > lengthOfOdd && lengthOfEven > currentLongestLength){
+                currentLongestLength = lengthOfEven;
+                indexOfLongestSubString = even;
             }
-            i--,j++;
         }
+        return s.substr(indexOfLongestSubString.first, indexOfLongestSubString.second - indexOfLongestSubString.first + 1);
     }
 };
 // @lc code=end
+
