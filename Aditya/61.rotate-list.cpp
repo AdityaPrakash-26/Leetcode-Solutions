23f5c7aab9456e9a10a9ae81ba4fe38ffa92193a
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-08 09:40:38 +0530

    q61

---
+++ Aditya/61.rotate-list.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=61 lang=cpp
+ *
+ * [61] Rotate List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(k==0 || head == nullptr){
+            return head;
+        }
+
+        //calculate size
+        ListNode* runner = head;
+        int size = 1;
+        while(runner->next != nullptr){
+            size++;
+            runner = runner->next;
+        }
+
+        k %= size;
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        //give headstart to fast
+        for(int i = 0; i<k; i++){
+            fast = fast->next;
+        }
+
+        while(fast->next != nullptr){
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // at this point, fast is point to the current tail
+        // and slow is pointing to the new tail.
+        fast->next = head;
+        head = slow->next;
+        slow->next = nullptr;
+
+        return head;
+    }
+};
+// @lc code=end
+
