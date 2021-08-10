00ca1ca9205993e98e70fdf47f4f53ff955b7fec
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-10 19:02:30 +0530

    q108

--- Aditya/0100-0999/108.convert-sorted-array-to-binary-search-tree.cpp
+++ Aditya/0100-0999/108.convert-sorted-array-to-binary-search-tree.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+31/31 cases passed (4 ms)
+Your runtime beats 99.6 % of cpp submissions
+Your memory usage beats 42.17 % of cpp submissions (21.5 MB)
+*/
+
+/*
  * @lc app=leetcode id=108 lang=cpp
  *
  * [108] Convert Sorted Array to Binary Search Tree
@@ -18,8 +25,29 @@
  */
 class Solution {
 public:
+    TreeNode* sortedArrayToBSTUtil(vector<int>& nums, int start, int end){
+        /* Base Case */
+        if (start > end){
+            return NULL;
+        }
+    
+        /* Get the middle element and make it root */
+        int mid = (start + end)/2;
+        TreeNode *root = new TreeNode(nums[mid]);
+    
+        /* Recursively construct the left subtree
+        and make it left child of root */
+        root->left = sortedArrayToBSTUtil(nums, start, mid - 1);
+    
+        /* Recursively construct the right subtree
+        and make it right child of root */
+        root->right = sortedArrayToBSTUtil(nums, mid + 1, end);
+    
+        return root;
+    }
+
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        
+        return sortedArrayToBSTUtil(nums, 0, nums.size()-1);
     }
 };
 // @lc code=end
