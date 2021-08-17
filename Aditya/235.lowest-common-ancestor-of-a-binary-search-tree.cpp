9a5e20b4fe765b8099a51a7f3c238c75f5931fd0
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-17 18:08:06 +0530

    q235 iterative version added

--- Aditya/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ Aditya/235.lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -34,5 +34,20 @@ public:
         }
         return root;
     }
+//iterative
+    TreeNode* lowestCommonAncestorIterative(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p -> val < cur -> val && q -> val < cur -> val) {
+                cur = cur -> left;
+            } else if (p -> val > cur -> val && q -> val > cur -> val) {
+                cur = cur -> right;
+            } else {
+                break;
+            }
+        }
+        return cur;
+    }
+
 };
 // @lc code=end
