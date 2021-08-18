5a47d6409e20569a0c10cfc80d84f910c080cfa8
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-18 12:32:15 +0530

    fixed indentation

--- Kashif/501-600/509.FibonacciNumber.cpp
+++ Kashif/501-600/509.FibonacciNumber.cpp
@@ -12,12 +12,12 @@
 using namespace std;
 
 int fib(int n) {
-        int f[n+2];
-        f[0] = 0;
-        f[1] = 1;
-        for(int i=2; i<=n; i++){
-            f[i] = f[i-1] + f[i-2];
-        }
-        
-        return f[n];
+    int f[n+2];
+    f[0] = 0;
+    f[1] = 1;
+    for(int i=2; i<=n; i++){
+        f[i] = f[i-1] + f[i-2];
     }
+    
+    return f[n];
+}
