93f17ba2c67ef39ea94f30bbdae72c216b0036e6
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-21 15:14:55 +0530

    q172

---
+++ Aditya/0100-0999/172.factorial-trailing-zeroes.cpp
@@ -0,0 +1,22 @@
+/*
+Accepted
+500/500 cases passed (0 ms)
+Your runtime beats 100 % of cpp submissions
+Your memory usage beats 95.31 % of cpp submissions (5.8 MB)
+*/
+
+/*
+ * @lc app=leetcode id=172 lang=cpp
+ *
+ * [172] Factorial Trailing Zeroes
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        return n==0 ? 0 : n/5 + trailingZeroes(n/5);
+    }
+};
+// @lc code=end
+
