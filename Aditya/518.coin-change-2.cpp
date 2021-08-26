bf71640c63419fac5d99877abc135fde6cd4af69
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-26 18:35:37 +0530

    q518

---
+++ Aditya/518.coin-change-2.cpp
@@ -0,0 +1,38 @@
+/*
+Accepted
+28/28 cases passed (10 ms)
+Your runtime beats 78.13 % of cpp submissions
+Your memory usage beats 83.64 % of cpp submissions (7.1 MB)
+*/
+
+/*
+ * @lc app=leetcode id=518 lang=cpp
+ *
+ * [518] Coin Change 2
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        // ways[i] += ways[i-denom]
+
+        vector<int> ways(amount+1);
+        
+        for(int i = 1; i<=amount; i++){
+            ways[i] = 0;
+        }
+
+        ways[0] = 1;
+
+        for(int i = 0; i<coins.size(); i++){
+            int denomination = coins[i];
+            for(int j = denomination; j < ways.size(); j++){
+                ways[j] += ways[j-denomination];
+            }
+        }
+        return ways[amount];
+    }
+};
+// @lc code=end
+
