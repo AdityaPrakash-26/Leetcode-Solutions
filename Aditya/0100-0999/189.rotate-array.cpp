1ec87022ecec5942e544c6b4cf69241e631bcbed
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-27 19:15:07 +0530

    q189

--- Aditya/0100-0999/189.rotate-array.cpp
+++ Aditya/0100-0999/189.rotate-array.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+37/37 cases passed (28 ms)
+Your runtime beats 66.71 % of cpp submissions
+Your memory usage beats 38.21 % of cpp submissions (25.1 MB)
+*/
+
+/*
  * @lc app=leetcode id=189 lang=cpp
  *
  * [189] Rotate Array
@@ -8,7 +15,12 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
+        vector<int> nums2 = nums;
+        int size = nums.size();
+        for(int i = 0; i<size; i++){
+            nums2[i] = nums[(size - k%size + i)%size];
+        }
+        nums = nums2;
     }
 };
 // @lc code=end
