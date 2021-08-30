866be139e4f65d2bdf7e9e92c7b4d900a25a8675
Aditya Prakash <prakashadityaap@gmail.com>
2021-08-30 09:45:28 +0530

    q11 revision

--- Aditya/0010-0099/11.container-with-most-water.cpp
+++ Aditya/0010-0099/11.container-with-most-water.cpp
@@ -1,9 +1,8 @@
-// @before-stub-for-debug-begin
-#include <vector>
-#include <string>
-
-using namespace std;
-// @before-stub-for-debug-end
+/*
+ * The widest container (using first and last line) is a good candidate, because of its width. Its water level is the height of the smaller one of first and last line.
+ * All other containers are less wide and thus would need a higher water level in order to hold more water.
+ * The smaller one of first and last line doesn't support a higher water level and can thus be safely removed from further consideration.
+*/
 
 /*
  * @lc app=leetcode id=11 lang=cpp
