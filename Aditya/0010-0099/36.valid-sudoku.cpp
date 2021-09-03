3f75cedf9712ed15387f4b59610aad562856a21f
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-03 16:56:03 +0530

    q36

--- Aditya/0010-0099/36.valid-sudoku.cpp
+++ Aditya/0010-0099/36.valid-sudoku.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+507/507 cases passed (44 ms)
+Your runtime beats 5.77 % of cpp submissions
+Your memory usage beats 26.48 % of cpp submissions (20.2 MB)
+*/
+
+/*
  * @lc app=leetcode id=36 lang=cpp
  *
  * [36] Valid Sudoku
@@ -8,8 +15,26 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        vector<set<int>> rows(9), cols(9), blocks(9);
         
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                
+                if (board[i][j] == '.') continue;
+                
+                int curr = board[i][j] - '0';
+
+                //we only need to get the index of the top left cell in each block
+                if (rows[i].count(curr) || cols[j].count(curr) || blocks[(i/3)*3+j/3].count(curr)) 
+                    return false;
+                
+                rows[i].insert(curr);
+                cols[j].insert(curr);
+                blocks[(i/3)*3+j/3].insert(curr);
+            }
+        }
+        
+        return true;
     }
 };
 // @lc code=end
-
