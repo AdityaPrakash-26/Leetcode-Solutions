921b643a02be2c06b7f93ea7dee98febf87d3bcc
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-06 12:39:35 +0530

    q451

---
+++ Aditya/0100-0999/451.sort-characters-by-frequency.cpp
@@ -0,0 +1,39 @@
+/*
+Accepted
+32/32 cases passed (12 ms)
+Your runtime beats 79.6 % of cpp submissions
+Your memory usage beats 5.63 % of cpp submissions (15.9 MB)
+*/
+
+/*
+ * @lc app=leetcode id=451 lang=cpp
+ *
+ * [451] Sort Characters By Frequency
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> freq;
+        vector<string> bucket(s.size()+1, "");
+        string res;
+        
+        //count frequency of each character
+        for(char c:s) freq[c]++;
+        //put character into frequency bucket
+        for(auto& it:freq) {
+            int n = it.second;
+            char c = it.first;
+            bucket[n].append(n, c);
+        }
+        //form descending sorted string
+        for(int i=s.size(); i>0; i--) {
+            if(!bucket[i].empty())
+                res.append(bucket[i]);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
