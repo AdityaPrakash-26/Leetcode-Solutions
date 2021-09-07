aeea324a5db7f8ffe638988bb6c21110149865c7
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-07 12:14:25 +0530

    q200

---
+++ Aditya/0100-0999/200.number-of-islands.cpp
@@ -0,0 +1,49 @@
+/*
+Accepted
+49/49 cases passed (32 ms)
+Your runtime beats 17.29 % of cpp submissions
+Your memory usage beats 13.52 % of cpp submissions (12.3 MB)
+*/
+
+/*
+ * @lc app=leetcode id=200 lang=cpp
+ *
+ * [200] Number of Islands
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void DFS(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size()){
+            return;
+        }
+
+        if(grid[i][j] == '0' || grid[i][j] == '2'){
+            return;
+        }
+
+        grid[i][j] = '2';   //mark as visited
+
+        DFS(grid, i-1, j);
+        DFS(grid, i+1, j);
+        DFS(grid, i, j-1);
+        DFS(grid, i, j+1);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
+
+        for(int i = 0; i<grid.size(); i++){
+            for(int j = 0; j<grid[0].size(); j++){
+                if(grid[i][j] == '1'){
+                    DFS(grid, i, j);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+};
+// @lc code=end
+
