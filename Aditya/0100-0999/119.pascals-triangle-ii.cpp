4302181bd5ae1f273bfed475c22bfd6158b69734
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-12 13:18:57 +0530

    q119

---
+++ Aditya/0100-0999/119.pascals-triangle-ii.cpp
@@ -0,0 +1,29 @@
+/*
+Accepted
+34/34 cases passed (0 ms)
+Your runtime beats 100 % of cpp submissions
+Your memory usage beats 80.62 % of cpp submissions (6.4 MB)
+*/
+
+/*
+ * @lc app=leetcode id=119 lang=cpp
+ *
+ * [119] Pascal's Triangle II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> v(rowIndex+1);
+        long temp = 1;
+        v[0] = v[rowIndex] = 1;    // first and last element of the rowIndex-th row
+        for(int i = 1, up = rowIndex, down = 1; i < rowIndex; i++, up--, down++){
+            temp = temp*up / down;
+            v[i] = temp;
+        }
+        return v;
+    }
+};
+// @lc code=end
+
