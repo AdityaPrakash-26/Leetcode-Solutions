38b8779c9fd2ff7790e4763f3702be8ad9209294
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-18 09:56:18 +0530

    q714

---
+++ Aditya/714.best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=714 lang=cpp
+ *
+ * [714] Best Time to Buy and Sell Stock with Transaction Fee
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int cash = 0, hold = -prices[0];
+        for (int i = 1; i < prices.size(); i++) {
+            cash = max(cash, hold + prices[i] - fee);
+            hold = max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+};
+// @lc code=end
+
