a8c856f486d42891b6714e5c8e16aafa24af5bc9
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-23 10:29:59 +0530

    q45 revision

--- Aditya/0010-0099/45.jump-game-ii.cpp
+++ Aditya/0010-0099/45.jump-game-ii.cpp
@@ -15,7 +15,11 @@ Your memory usage beats 72.28 % of cpp submissions (16.4 MB)
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = size(nums), i = 0, maxReachable = 0, lastJumpedPos = 0, jumps = 0;
+        int n = nums.size();
+        int i = 0;
+        int maxReachable = 0;
+        int lastJumpedPos = 0;
+        int jumps = 0;
         while(lastJumpedPos < n - 1) {  // loop till last jump hasn't taken us till the end
             maxReachable = max(maxReachable, i + nums[i]);  // furthest index reachable on the next level from current level
             if(i == lastJumpedPos) {			  // current level has been iterated & maxReachable position on next level has been finalised
