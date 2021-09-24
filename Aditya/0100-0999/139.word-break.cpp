19f548627b6919d421a77380283526962238b124
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-24 09:24:13 +0530

    q139 revision

--- Aditya/0100-0999/139.word-break.cpp
+++ Aditya/0100-0999/139.word-break.cpp
@@ -12,12 +12,14 @@ public:
         dp[0] = true;
         
         // we mark as true every index that we managed to segment so far
-        for (int i = 1; i <= s.size(); i++)
-            for (int j = 0; j < i; j++)
+        for (int i = 1; i <= s.size(); i++) { //used for changing the size of the string being considered
+            for (int j = 0; j < i; j++) { // used for searching for the current string in `wordDict`
                 if ((dp[j]) && (find(wordDict.begin(), wordDict.end(), s.substr(j, i-j)) != wordDict.end())) {
                     dp[i] = true;
                     break;
                 }
+            }
+        }
         return dp.back();
     }
 };
