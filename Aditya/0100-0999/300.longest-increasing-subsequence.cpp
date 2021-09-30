c468156cba4a677dd6444969a4839427d05ce534
Aditya Prakash <prakashadityaap@gmail.com>
2021-09-30 19:17:00 +0530

    q300

--- Aditya/0100-0999/300.longest-increasing-subsequence.cpp
+++ Aditya/0100-0999/300.longest-increasing-subsequence.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+54/54 cases passed (260 ms)
+Your runtime beats 46.14 % of cpp submissions
+Your memory usage beats 62.18 % of cpp submissions (10.5 MB)
+*/
+
+/*
  * @lc app=leetcode id=300 lang=cpp
  *
  * [300] Longest Increasing Subsequence
@@ -7,27 +14,25 @@
 // @lc code=start
 class Solution {
 public:
-    vector<vector<int>> dp;
     int lengthOfLIS(vector<int>& nums) {
-        dp.resize(size(nums), vector<int>(1+size(nums), -1));   // dp[i][j] denotes max LIS starting from i when nums[j] is previous picked element
-        return solve(nums, 0, -1);
-    }
-    int solve(vector<int>& nums, int i, int prev_i) {
-        if(i >= size(nums)) {
-            return 0;
+        int size = nums.size();
+        vector<int> dp(size, 1);
+
+        for(int i = size - 1; i >= 0; i--){
+            for(int j = i+1; j<size; j++){
+                if(nums[i] < nums[j]){
+                    dp[i] = max(dp[i], 1 + dp[j]);
+                }
+            }
         }
 
-        if(dp[i][prev_i+1] != -1) { //memoization check
-            return dp[i][prev_i+1];
+        int maxElement = INT_MIN;
+        for(int i = 0; i < size; i++){
+            if(dp[i] > maxElement){
+                maxElement = dp[i];
+            }
         }
-        int take = 0;
-        int dontTake = solve(nums, i + 1, prev_i);
-        if(prev_i == -1 || nums[i] > nums[prev_i]) {
-            // try picking current element if no previous element is chosen or
-            // current > nums[prev_i]
-            take = 1 + solve(nums, i + 1, i); 
-        }
-        return dp[i][prev_i+1] = max(take, dontTake);
+        return maxElement;
     }
 };
 // @lc code=end
