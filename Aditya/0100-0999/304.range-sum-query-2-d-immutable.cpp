7024abd5e677cab78f6db38fa9bdcca949e05e31
Aditya Prakash <prakashadityaap@gmail.com>
2021-10-02 18:54:15 +0530

    q304

--- Aditya/0100-0999/304.range-sum-query-2-d-immutable.cpp
+++ Aditya/0100-0999/304.range-sum-query-2-d-immutable.cpp
@@ -1,8 +1,8 @@
 /*
 Accepted
-24/24 cases passed (501 ms)
-Your runtime beats 30.78 % of cpp submissions
-Your memory usage beats 60.8 % of cpp submissions (148 MB)
+24/24 cases passed (376 ms)
+Your runtime beats 59.86 % of cpp submissions
+Your memory usage beats 60.8 % of cpp submissions (147.9 MB)
 */
 
 /*
@@ -13,33 +13,20 @@ Your memory usage beats 60.8 % of cpp submissions (148 MB)
 
 // @lc code=start
 class NumMatrix {
-private:
-    int row, col;
-    vector<vector<int>> sums;
 public:
-    NumMatrix(vector<vector<int>> &matrix) {
-        row = matrix.size();
-        col = row > 0 ? matrix[0].size() : 0;
-        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-
-        for(int j = 1; j < col; j++) {
-            sums[0][j] = matrix[0][j] + sums[0][j - 1];
-        }
-        
-        // Pre process first col
-        for(int i = 1; i < row; i++) {
-            sums[i][0] = matrix[i][0] + sums[i - 1][0];
-        }
-
-        for(int i=1; i<=row; i++) {
-            for(int j=1; j<=col; j++) {
-                sums[i][j] = matrix[i-1][j-1] + sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
+    vector<vector<int>> sum;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        sum = vector<vector<int>>(m + 1, vector<int>(n + 1)); // sum[i][j] is sum of all elements inside the rectangle [0,0,i,j]
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];   // subtracting i-1 & j-1 bec we added it twice
             }
         }
     }
 
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        return sum[r2 + 1][c2 + 1] - sum[r2 + 1][c1] - sum[r1][c2 + 1] + sum[r1][c1]; // adding r1 & c1 bec. we subtracted it twice
     }
 };
 
