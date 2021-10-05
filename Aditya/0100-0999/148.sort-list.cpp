5625991516056f27d8611c2868a0f6b24bd8315f
Aditya Prakash <prakashadityaap@gmail.com>
2021-10-05 09:43:26 +0530

    q148

---
+++ Aditya/0100-0999/148.sort-list.cpp
@@ -0,0 +1,67 @@
+/*
+Accepted
+28/28 cases passed (119 ms)
+Your runtime beats 35.82 % of cpp submissions
+Your memory usage beats 97.94 % of cpp submissions (29 MB)
+*/
+
+/*
+ * @lc app=leetcode id=148 lang=cpp
+ *
+ * [148] Sort List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next)
+            return head;
+        ListNode* mid = getMid(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummyHead(0);
+        ListNode* ptr = &dummyHead;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                ptr->next = list1;
+                list1 = list1->next;
+            } else {
+                ptr->next = list2;
+                list2 = list2->next;
+            }
+            ptr = ptr->next;
+        }
+        if(list1) ptr->next = list1;
+        else ptr->next = list2;
+
+        return dummyHead.next;
+    }
+
+    ListNode* getMid(ListNode* head) {
+        ListNode* midPrev = nullptr;
+        while (head && head->next) {
+            midPrev = (midPrev == nullptr) ? head : midPrev->next;
+            head = head->next->next;
+        }
+        ListNode* mid = midPrev->next;
+        midPrev->next = nullptr;
+        return mid;
+    }
+};
+// @lc code=end
+
