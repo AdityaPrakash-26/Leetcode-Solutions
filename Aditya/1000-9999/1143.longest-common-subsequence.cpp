0b3288ba2be7df130ef7a4eb48a2628b7cebdb0c
Aditya Prakash <prakashadityaap@gmail.com>
2021-10-05 13:42:13 +0530

    q1143

--- Aditya/1000-9999/1143.longest-common-subsequence.cpp
+++ Aditya/1000-9999/1143.longest-common-subsequence.cpp
@@ -15,7 +15,7 @@ Your memory usage beats 32.32 % of cpp submissions (13 MB)
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> m(text1.size() + 1, vector<int>(text2.size() + 1));
+        vector<vector<int>> m(text1.size() + 1, vector<int>(text2.size() + 1, 0));
         for (int i = 1; i <= text1.size(); ++i){
             for (int j = 1; j <= text2.size(); ++j){
                 if (text1[i - 1] == text2[j - 1]){ 
