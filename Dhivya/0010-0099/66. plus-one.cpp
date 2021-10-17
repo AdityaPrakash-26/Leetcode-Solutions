8d1cd35045df97aea6f300a960bd9b3060ef1c09
Dhivya S <dhivyasreedhar@gmail.com>
2021-10-17 15:25:46 +0530

    Create 66. plus-one.cpp

---
+++ Dhivya/0010-0099/66. plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {digits[i]++;
+            return digits;}
+            
+            else
+                digits[i]=0;
+        }
+        
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+   
+};
