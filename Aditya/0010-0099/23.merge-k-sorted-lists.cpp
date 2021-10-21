2b4e62e62c84169e4078b7dbee2bf4e422e076d0
Aditya Prakash <prakashadityaap@gmail.com>
2021-10-21 10:21:25 +0530

    q23 revision

--- Aditya/0010-0099/23.merge-k-sorted-lists.cpp
+++ Aditya/0010-0099/23.merge-k-sorted-lists.cpp
@@ -24,29 +24,26 @@ Your memory usage beats 45.63 % of cpp submissions (13.4 MB)
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
         if(lists.empty()){
             return nullptr;
         }
-        int len = lists.size();
-        while (len > 1) {
-            for (int i = 0; i < len / 2; ++i) {
-                lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
-            }
-            len = (len + 1) / 2;
+
+        while(lists.size() > 1){
+            lists.push_back(mergeTwoLists(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
         }
-        
+
         return lists.front();
     }
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         if(l1 == nullptr){
             return l2;
         }
-
         if(l2 == nullptr){
             return l1;
         }
-
         if(l1->val <= l2->val){
             l1->next = mergeTwoLists(l1->next, l2);
             return l1;
