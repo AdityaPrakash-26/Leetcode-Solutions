d2545da42a0cc043837ad26978c1058f1b7f76bc
Aditya Prakash <prakashadityaap@gmail.com>
2021-11-05 08:58:39 +0530

    q105 revision

--- Aditya/0100-0999/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ Aditya/0100-0999/105.construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -18,20 +18,20 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& P, vector<int>& I) {
-        unordered_map<int, int> M;
-        for (int i = 0; i < I.size(); i++)
-            M[I[i]] = i;
-        return splitTree(P, M, 0, 0, I.size()-1);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> inorderToIndexMap;  //
+        for (int i = 0; i < inorder.size(); i++)
+            inorderToIndexMap[inorder[i]] = i;
+        return splitTree(preorder, inorderToIndexMap, 0, 0, inorder.size()-1);
     }
     
-    TreeNode* splitTree(vector<int>& P, unordered_map<int, int>& M, int pix, int ileft, int iright) {
-        int rval = P[pix], imid = M[rval];
-        TreeNode* root = new TreeNode(rval);            
+    TreeNode* splitTree(vector<int>& preorder, unordered_map<int, int>& inorderToIndexMap, int preIdx, int ileft, int iright) {
+        int rootVal = preorder[preIdx], imid = inorderToIndexMap[rootVal];
+        TreeNode* root = new TreeNode(rootVal);            
         if (imid > ileft)
-            root->left = splitTree(P, M, pix+1, ileft, imid-1);
+            root->left = splitTree(preorder, inorderToIndexMap, preIdx+1, ileft, imid-1);
         if (imid < iright)
-            root->right = splitTree(P, M, pix+imid-ileft+1, imid+1, iright);
+            root->right = splitTree(preorder, inorderToIndexMap, preIdx+imid-ileft+1, imid+1, iright);
         return root;
     }
 };
