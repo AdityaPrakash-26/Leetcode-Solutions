52ce7b2e84c9d17ac091064346879c923ce09d46
Aditya Prakash <prakashadityaap@gmail.com>
2021-11-16 11:56:59 +0530

    q75 revision

--- Aditya/0010-0099/75.sort-colors.cpp
+++ Aditya/0010-0099/75.sort-colors.cpp
@@ -15,18 +15,20 @@ Your memory usage beats 90.91 % of cpp submissions (8.2 MB)
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zeroPos = 0;
-        int twoPos = nums.size() - 1;
-        
-        for(int i = 0; i <= twoPos; i++){
-            if(nums[i] == 0){
-                swap(nums[i], nums[zeroPos++]);
-            } else if(nums[i] == 2){
-                swap(nums[i--], nums[twoPos--]);    // the previous i+1 now became i, so it is
-                                                    // not checked as i will get
-                                                    // incremented in the next
-                                                    // interation. Hence,
-                                                    // decrement i.
+        int leftIdx = 0;
+        int midIdx = 0;
+        int rightIdx = nums.size() - 1;
+
+        while(midIdx <= rightIdx){
+            if(nums[midIdx] == 0){
+                swap(nums[leftIdx], nums[midIdx]);
+                leftIdx++;
+                midIdx++;
+            } else if (nums[midIdx] == 1){
+                midIdx++;
+            } else if (nums[midIdx] == 2){
+                swap(nums[midIdx], nums[rightIdx]);
+                rightIdx--;
             }
         }
     }
