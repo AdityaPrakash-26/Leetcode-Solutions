93c6b37d83155ab15a110651a2d952be9d0686f1
Aditya Prakash <prakashadityaap@gmail.com>
2021-12-12 08:45:48 +0530

    q416

---
+++ Aditya/416.partition-equal-subset-sum.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=416 lang=cpp
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (auto a : nums) // Sum up the array
+            sum += a;
+        
+        if (sum % 2) // If the sum is odd - we can never find two equal partitions
+            return false;
+        
+        sum /= 2;
+        vector<bool> dp(sum+1, false); // dp keeps for each number if it has a subset or not
+        dp[0] = true;
+        
+        for (auto a : nums) {
+            for (int i = sum; i >= a; i--) {
+                dp[i] = dp[i] || dp[i-a]; // for each number, either we use it or we don't
+            } 
+        }
+        return dp[sum];
+    }
+};
+// @lc code=end
+
