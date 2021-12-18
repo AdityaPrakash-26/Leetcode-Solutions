1d52de78e79d1ea1ec81d97db1b36850e36d755a
Aditya Prakash <prakashadityaap@gmail.com>
2021-12-18 08:42:34 +0530

    q902

---
+++ Aditya/902.numbers-at-most-n-given-digit-set.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=902 lang=cpp
+ *
+ * [902] Numbers At Most N Given Digit Set
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& D, int N) {
+        string NS = to_string(N);
+        int digit = NS.size(), dsize = D.size(), rtn = 0;
+        
+        for (int i = 1 ; i < digit ; ++i)
+            rtn += pow(dsize, i);
+        
+        for (int i = 0 ; i < digit ; ++i) {
+            bool hasSameNum = false;
+            for (string &d : D) {
+                if (d[0] < NS[i]) 
+                    rtn += pow(dsize, digit - i - 1);
+                else if (d[0] == NS[i]) 
+                    hasSameNum = true;
+            }
+            if (!hasSameNum) return rtn;
+        }               
+        return rtn+1;
+    }
+};
+// @lc code=end
+
