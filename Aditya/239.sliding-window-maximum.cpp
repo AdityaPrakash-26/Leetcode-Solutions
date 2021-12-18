04af21f9309e60e2f9f91588b1d3984565bd8396
Aditya Prakash <prakashadityaap@gmail.com>
2021-12-18 11:24:36 +0530

    q239

---
+++ Aditya/239.sliding-window-maximum.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=239 lang=cpp
+ *
+ * [239] Sliding Window Maximum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector <int> ans;
+        int n=nums.size();
+        deque <int> q; //only indices are stored
+        for(int i=0;i<n;i++){   
+            while(!q.empty() && i-q.front()>=k){
+                q.pop_front(); //only window size of k is allowed
+            }
+            
+            while(!q.empty() && nums[q.back()]<nums[i]){
+                q.pop_back();
+            }
+
+            q.push_back(i);
+
+            if(i>=k-1) {
+                ans.push_back(nums[q.front()]); //our max value in O(1)
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
