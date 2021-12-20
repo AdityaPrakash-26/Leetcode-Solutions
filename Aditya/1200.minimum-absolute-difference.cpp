f023854fe2f2b664d7f80bb5f1fc0039932c5111
Aditya Prakash <prakashadityaap@gmail.com>
2021-12-20 09:09:17 +0530

    q1200

---
+++ Aditya/1200.minimum-absolute-difference.cpp
@@ -0,0 +1,31 @@
+
+/*
+ * @lc app=leetcode id=1200 lang=cpp
+ *
+ * [1200] Minimum Absolute Difference
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int minDiff = INT_MAX;
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++){
+            int diff = abs(nums[i] - nums[i-1]);
+            if(diff < minDiff){
+                minDiff = diff;
+                ans.clear();
+                ans.push_back({nums[i-1], nums[i]});
+            } else if (diff == minDiff){
+                ans.push_back({nums[i-1], nums[i]});
+            } else {
+                continue;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
