5be499664c346e6fe31360cd9a223364b4233d17
Aditya Prakash <prakashadityaap@gmail.com>
2021-12-29 07:55:12 +0530

    q116

--- Aditya/0100-0999/116.populating-next-right-pointers-in-each-node.cpp
+++ Aditya/0100-0999/116.populating-next-right-pointers-in-each-node.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+58/58 cases passed (16 ms)
+Your runtime beats 88.46 % of cpp submissions
+Your memory usage beats 36.16 % of cpp submissions (17.2 MB)
+*/
+
+/*
  * @lc app=leetcode id=116 lang=cpp
  *
  * [116] Populating Next Right Pointers in Each Node
