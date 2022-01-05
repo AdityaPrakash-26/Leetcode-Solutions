fa5b329e1634e0af5dd485f7499f667a9f33a0a7
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-05 07:49:53 +0530

    q131

---
+++ Aditya/89.gray-code.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=89 lang=cpp
+ *
+ * [89] Gray Code
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        
+    }
+};
+// @lc code=end
+
