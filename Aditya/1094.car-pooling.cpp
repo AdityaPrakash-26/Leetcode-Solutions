24f76858e5d52bcc97f124178887c18e5afe0898
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-06 08:27:50 +0530

    q1094

---
+++ Aditya/1094.car-pooling.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1094 lang=cpp
+ *
+ * [1094] Car Pooling
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<pair<int, int>> v;
+        
+        for(int i = 0; i < trips.size(); i++){
+            v.push_back({trips[i][1], trips[i][0]}); // picking up passengers
+            v.push_back({trips[i][2], -trips[i][0]}); // dropping them off
+        }
+        
+        sort(v.begin(), v.end());
+        
+        int filled = 0;
+        
+        for(int i = 0; i < v.size(); i++){
+            filled += v[i].second;
+            if(filled > capacity) return false;
+        }
+        return true;
+    }
+};
+// @lc code=end
+
