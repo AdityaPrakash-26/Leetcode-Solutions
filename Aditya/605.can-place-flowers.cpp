7caab2593af55d2c95a2a1b9f86641ea38fe12c9
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-18 09:33:14 +0530

    q605

---
+++ Aditya/605.can-place-flowers.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=605 lang=cpp
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int i = 0, count = 0;
+        while (i < flowerbed.size()) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i++] = 1;
+                count++;
+            }
+            if(count>=n)
+                return true;
+            i++;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
