4a5db6c4aa279ccbc1c7f304510d9e2d54921bdb
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-25 12:09:52 +0530

    q941

--- Aditya/941.valid-mountain-array.cpp
+++ Aditya/941.valid-mountain-array.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+53/53 cases passed (28 ms)
+Your runtime beats 75.02 % of cpp submissions
+Your memory usage beats 79.54 % of cpp submissions (22.4 MB)
+*/
+
+/*
  * @lc app=leetcode id=941 lang=cpp
  *
  * [941] Valid Mountain Array
