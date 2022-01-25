3f4c11e95399ab6f9a0b8f5e856dc10f93f2c12b
Aditya Prakash <prakashadityaap@gmail.com>
2022-01-25 12:09:09 +0530

    q941

---
+++ Aditya/941.valid-mountain-array.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=941 lang=cpp
+ *
+ * [941] Valid Mountain Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        int n = A.size(), i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) i++;
+        while (j > 0 && A[j - 1] > A[j]) j--;
+        return i > 0 && i == j && j < n - 1;
+    }
+};
+// @lc code=end
+
