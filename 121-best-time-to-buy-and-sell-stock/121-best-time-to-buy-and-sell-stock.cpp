28ea4c5700daa14baef360c39f549f65a0b29796
Kashif Raza <kashifraza08012001@gmail.com>
2022-02-01 21:15:44 +0530

    Time: 223 ms (24.66%), Space: 93.4 MB (53.45%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -3,16 +3,14 @@ public:
     int maxProfit(vector<int>& prices) {
         if(prices.size() == 1) return 0;
         int maxProfit = 0;
-        stack<int> stk;
-        stk.push(prices[0]);
+        int check = prices[0];
         int i = 1;
         while(i < prices.size()){
-            int stkTop = stk.top();
-            if(prices[i] < stkTop){
-                stk.pop();
-                stk.push(prices[i]);
+            //int stkTop = stk.top();
+            if(prices[i] < check){
+                check = prices[i];
             }else{
-                int diff = (prices[i] - stkTop);
+                int diff = (prices[i] - check);
                 maxProfit = max(maxProfit, diff);
             }
             i++;
