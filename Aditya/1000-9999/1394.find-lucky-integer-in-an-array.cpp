80bd91ead075af34507d4557adc4b843284050a8
Aditya Prakash <prakashadityaap@gmail.com>
2022-03-11 09:49:13 +0530

    q1394

--- Aditya/1000-9999/1394.find-lucky-integer-in-an-array.cpp
+++ Aditya/1000-9999/1394.find-lucky-integer-in-an-array.cpp
@@ -1,4 +1,11 @@
 /*
+Accepted
+100/100 cases passed (7 ms)
+Your runtime beats 74.95 % of cpp submissions
+Your memory usage beats 55.8 % of cpp submissions (10.2 MB)
+*/
+
+/*
  * @lc app=leetcode id=1394 lang=cpp
  *
  * [1394] Find Lucky Integer in an Array
