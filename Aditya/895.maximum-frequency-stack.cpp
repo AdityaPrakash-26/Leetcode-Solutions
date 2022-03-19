08d9a3868d154c0f3716aafb5c00b845fc2c70ee
Aditya Prakash <prakashadityaap@gmail.com>
2022-03-19 08:36:43 +0530

    q895

---
+++ Aditya/895.maximum-frequency-stack.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=895 lang=cpp
+ *
+ * [895] Maximum Frequency Stack
+ */
+
+// @lc code=start
+class FreqStack {
+public:
+    unordered_map<int, int> fmap;
+    vector<vector<int>> fstack = {{}};
+    
+    void push(int x) {
+        int freq = fmap[x] + 1;
+        fmap[x] = freq;
+        if (freq == fstack.size()) fstack.push_back(vector<int>());
+        fstack[freq].push_back(x);
+    }
+    
+    int pop() {
+        int x = fstack.back().back();
+        fstack.back().pop_back();
+        if (fstack.back().empty()) fstack.pop_back();
+        fmap[x]--;
+        return x;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(val);
+ * int param_2 = obj->pop();
+ */
+// @lc code=end
+
