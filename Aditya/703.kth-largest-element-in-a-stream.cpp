e0756b2c8e000c19c04122d5a63c1d5d0997ef2d
Aditya Prakash <prakashadityaap@gmail.com>
2022-04-16 10:16:07 +0530

    q703

---
+++ Aditya/703.kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=703 lang=cpp
+ *
+ * [703] Kth Largest Element in a Stream
+ */
+
+// @lc code=start
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> q;
+    int size;
+    KthLargest(int k, vector<int>& nums) {
+        size = k;
+        for(int i = 0; i < nums.size(); i++){
+            q.push(nums[i]);
+            if(q.size() > k) q.pop();
+        }
+    }
+    
+    int add(int val) {
+        q.push(val);
+        if(q.size() > size) {
+            q.pop();
+        }
+        return q.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+// @lc code=end
+
